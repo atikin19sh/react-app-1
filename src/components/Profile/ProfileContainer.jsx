7d0c3f244e4dc2c314dcf93b14dfd41efb09d0c5
atikin19sh <atikin19sh@gmail.com>
2022-07-04 20:50:56 +0500

    withRouter

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import React from "react";
 import { connect } from "react-redux";
+import { useLocation, useParams, useNavigate } from 'react-router-dom';
 import { setUserProfile }
   from "../../redux/profile-reducer";
 import Profile from "./Profile";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+    let userId = this.props.router.params.userId;
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then(response => {
         this.props.setUserProfile(response.data);
       })
@@ -24,4 +26,20 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return (
+      <Component
+        {...props}
+        router={{ location, navigate, params }}
+      />
+    );
+  }
+
+  return ComponentWithRouterProp;
+}
+
+export default connect(mapStateToProps, { setUserProfile })(withRouter(ProfileContainer));
