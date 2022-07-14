97a0ec237298f374782f045512890a1bed293b0e
XeNoX <atikin19sh@gmail.com>
2022-07-14 17:55:32 +0500

    redux-form (1)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+const LoginForm = (props) => {
+  return <form onSubmit={props.handleSubmit}>
+    <div>
+      <Field component='input' name='login' type='text' placeholder='Login' />
+    </div>
+    <div>
+      <Field component='input' name='password' type='text' placeholder='Password' />
+    </div>
+    <div>
+      <Field component='input' name='rememberMe' type='checkbox' />remember me
+    </div>
+    <div>
+      <button type='submit'>
+        Log In
+      </button>
+    </div>
+  </form>
+}
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
 
 const LoginPage = (props) => {
-  return <>
-    <h1>LOGIN PAGE</h1>
-    <a href='https://social-network.samuraijs.com/login'>Please log in to continue</a>
-  </>
+  const onSubmit = (formData) => {
+    console.log(formData)
+  }
+
+  return <div>
+    <h1>Login</h1>
+    <LoginReduxForm onSubmit={onSubmit}/>
+  </div>
 }
 
 export default LoginPage;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,13 +5,15 @@ import dialogsReducer from "./dialogs-reducer";
 import usersReducer from './users-reducer';
 import sidebarReducer from "./sidebar-reducer";
 import authReducer from './auth-reducer';
+import {reducer as formReducer} from 'redux-form';
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   usersPage: usersReducer,
   sidebar: sidebarReducer,
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
